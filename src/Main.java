5061a1399a69894d8465b5c9327c43d49c5dd1c5
Prak-Pisith <31238364+Prak-Pisith@users.noreply.github.com>
2018-03-13 15:01:12 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello all Group members");
         System.out.println("Hello Group 4");
         System.out.println("Hello world");
-        System.out.println("Hello Everyone");
+       
     }
 
 }
