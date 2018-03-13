41cffdb32992a40df2d9151c8a9bc4df6d13d57e
Prak-Pisith <31238364+Prak-Pisith@users.noreply.github.com>
2018-03-13 15:13:05 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,10 @@ public class Main {
         System.out.println("Hello all Group members");
         System.out.println("Hello Group 4");
         System.out.println("Hello world");
-        new Main();
-    }
-    public Main(){
         show();
     }
-    public void show(){
+
+    public static void show(){
         System.out.println("This is my function");
     }
 
